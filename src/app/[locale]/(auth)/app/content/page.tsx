2069ba2368d59b4711e3f8ec804e0fc72ac4c9d1
Taimoor Khan <27800340+Taimoormk@users.noreply.github.com>
2024-06-01 22:33:59 +0500

    :recycle: updated content

--- src/app/[locale]/(auth)/app/content/page.tsx
+++ src/app/[locale]/(auth)/app/content/page.tsx
@@ -5,18 +5,20 @@ import { Typography } from '@/components/material-tailwind';
 const Content = (): ReactElement => {
   return (
     <div className='grid grid-cols-3 gap-6'>
-      {googleServicesContent.map(({ id, title, content }) => {
-        return (
-          <div key={id} className='p-4 border border-white rounded-md'>
-            <Typography key={id} variant='h3' className='pb-4'>
-              {title}
-            </Typography>
-            <Typography variant='paragraph'>
-              {content.slice(0, 300)}
-            </Typography>
-          </div>
-        );
-      })}
+      {googleServicesContent
+        .sort((a, b) => a.order - b.order)
+        .map(({ id, title, content }) => {
+          return (
+            <div key={id} className='p-4 border border-white rounded-md'>
+              <Typography key={id} variant='h3' className='pb-4'>
+                {title}
+              </Typography>
+              <Typography variant='paragraph'>
+                {content.slice(0, 300)}
+              </Typography>
+            </div>
+          );
+        })}
     </div>
   );
 };

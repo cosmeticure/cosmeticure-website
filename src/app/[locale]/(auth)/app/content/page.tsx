e82400adc8b5c18b5d846c221cfdf712b866bac2
Taimoor Khan <27800340+Taimoormk@users.noreply.github.com>
2024-06-01 23:02:04 +0500

    :recycle: google products

--- src/app/[locale]/(auth)/app/content/page.tsx
+++ src/app/[locale]/(auth)/app/content/page.tsx
@@ -1,24 +1,48 @@
 import { ReactElement } from 'react';
-import { googleServicesContent } from '@/data/content';
+import { googleProductsContent, googleServicesContent } from '@/data/content';
 import { Typography } from '@/components/material-tailwind';
 
 const Content = (): ReactElement => {
   return (
-    <div className='grid grid-cols-3 gap-6'>
-      {googleServicesContent
-        .sort((a, b) => a.order - b.order)
-        .map(({ id, title, content }) => {
-          return (
-            <div key={id} className='p-4 border border-white rounded-md'>
-              <Typography key={id} variant='h3' className='pb-4'>
-                {title}
-              </Typography>
-              <Typography variant='paragraph'>
-                {content.slice(0, 300)}
-              </Typography>
-            </div>
-          );
-        })}
+    <div>
+      <Typography className='p-4' variant='h3'>
+        Services published on Google
+      </Typography>
+      <div className='grid grid-cols-3 gap-6'>
+        {googleServicesContent
+          .sort((a, b) => a.order - b.order)
+          .map(({ id, title, content }) => {
+            return (
+              <div key={id} className='p-4 border border-white rounded-md'>
+                <Typography key={id} variant='h4' className='pb-4'>
+                  {title}
+                </Typography>
+                <Typography variant='paragraph'>
+                  {content.slice(0, 300)}
+                </Typography>
+              </div>
+            );
+          })}
+      </div>
+      <Typography className='p-4' variant='h3'>
+        Products published on Google
+      </Typography>
+      <div className='grid grid-cols-3 gap-6'>
+        {googleProductsContent
+          .sort((a, b) => a.order - b.order)
+          .map(({ brand, content, id, price, title }) => {
+            return (
+              <div key={id} className='p-4 border border-white rounded-md'>
+                <Typography key={id} variant='h4' className='pb-4'>
+                  {title} - {brand}
+                </Typography>
+                <Typography variant='paragraph'>
+                  {content.slice(0, 300)} - {price}
+                </Typography>
+              </div>
+            );
+          })}
+      </div>
     </div>
   );
 };
